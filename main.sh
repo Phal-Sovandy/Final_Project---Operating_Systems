a6dd85fd109c730a08ccf61eb77a6e28eaaf7f0c
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-03-15 15:46:08 +0700

    Implement main program logic with user menu for file management operations and logging

--- main.sh
+++ main.sh
@@ -2,4 +2,33 @@
 # This the main program file
 
 # Additional: Keep a log file of all actions performed by users in a file called script.log in the same directory where you run the program.
-bash listing_details.sh
+ 
+ script_file="$(pwd)/script.log"
+
+ echo "$(date) --- PROGRAM STARTED..." >> "$script_file"
+ 
+ while true; do 
+    echo "================================"
+    echo "        File Management"
+    echo "================================"
+    echo "1. List all files"
+    echo "2. Create backups"
+    echo "3. Count files within directory"
+    echo "4. Show disk usage"
+    echo "5. Search for file"
+    echo "6. Compress file or directory"
+    echo "7. Exit"
+    echo "--------------------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) bash ./scripts/listing_details.sh;;
+        2) bash ./scripts/backing_up.sh;; 
+        3) bash ./scripts/count_number_files.sh;;
+        4) bash ./scripts/display_disk.sh;;
+        5) bash ./scripts/search_for_file.sh;;
+        6) bash ./scripts/compress.sh;;
+        7) exit 0;;
+        *) echo "Invalid Choice";;
+    esac
+done
