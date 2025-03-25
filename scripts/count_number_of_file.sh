bca637c98d569c5e361f6fa41116fc07ad1bf6d5
Hengsreng2409 <hengsreng.toek@student.cadt.edu.kh>
2025-03-25 10:57:59 +0700

    Update count_number_of_file.sh

--- scripts/count_number_of_file.sh
+++ scripts/count_number_of_file.sh
@@ -1,3 +1,16 @@
 #!bin/bash
 
 # Count the number of files in a directory of a given directory.
+
+script_file="$(pwd)/script.log" # Log file
+
+read -p "Enter directory path: " dir
+if [ -d "$dir" ]; then
+    # List all file within directory and sub-directory and count all the files
+    count=$(find "$dir" -type f | wc -l)
+    echo "Number of files: $count"
+    # Write message to log file
+    echo "$(date) - Counted $count files in $dir" >> "$script_file"
+else
+    echo "Directory does not exist."
+fi
