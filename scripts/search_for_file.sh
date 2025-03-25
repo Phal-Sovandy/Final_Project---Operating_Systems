9c4b137ce8efa3e5b84607e3fda66e275c34cdd7
monotai <161217184+monotai@users.noreply.github.com>
2025-03-25 10:45:03 +0700

    Update search_for_file.sh

--- scripts/search_for_file.sh
+++ scripts/search_for_file.sh
@@ -1,3 +1,16 @@
 #!bin/bash
 
 # Search for a file by name or extension of a given directory.
+
+script_file="$(pwd)/script.log" # Log file
+
+read -p "Enter directory path: " dir
+read -p "Enter filename or extension to search: " search
+if [ -d "$dir" ]; then
+    # Insensitive case search (eg. *txt, *TxT,...)
+    find "$dir" -iname "$search"
+    # Write message to log file
+    echo "$(date) - Searched for $search in $dir" >> "$script_file"
+else
+    echo "Directory does not exist."
+fi
