a62c2f5750bba8706fbf501c559e97a432be7d63
monotai <161217184+monotai@users.noreply.github.com>
2025-03-25 10:45:32 +0700

    Update compress.sh

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,3 +1,15 @@
 #!bin/bash
 
 # Compress the given files or directories.
+
+script_file="$(pwd)/script.log" # Log file
+
+read -p "Enter file or directory to compress: " src
+read -p "Enter name for compressed file (without extension): " dest
+if [ -e "$src" ]; then
+    tar -czf "$dest.tar.gz" "$src"
+    # Write message to log file
+    echo "$(date) - Compressed $src into $dest.tar.gz" >> "$script_file"
+else
+    echo "Source does not exist."
+fi
